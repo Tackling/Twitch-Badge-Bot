3753e1ea4563fd74500459f553e73822e8b41984
Rv <129611072+Tackling@users.noreply.github.com>
2023-06-21 23:15:29 +1000

    config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  clientId: 'YOUR_TWITCH_CLIENT_ID', // Replace with your Twitch client ID
+  clientSecret: 'YOUR_TWITCH_CLIENT_SECRET', // Replace with your Twitch client secret
+  roleId: 'YOUR_DISCORD_ROLE_ID', // Replace with the actual role ID you want to mention
+  webhookUrl: 'YOUR_DISCORD_WEBHOOK_URL' // Replace with your Discord webhook URL
+};
